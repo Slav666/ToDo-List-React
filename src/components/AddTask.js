03fa35e5544f8995879a2ca963874f4c5257b880
Slav666 <slawomirdyk@gmail.com>
2019-11-01 23:21:44 +0000

    Class AddTask created

---
+++ src/components/AddTask.js
@@ -0,0 +1,15 @@
+
+import React, {Component} from 'react';
+
+class  AddTask extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div className="form">
+                <div>Add task</div>
+            </div>
+         );
+    }
+}
+ 
+export default AddTask;
--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-
 import './App.css';
+import AddTask from './AddTask';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
       ToDo List
+      <AddTask/>
       </div>
     );
   }
