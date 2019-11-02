e41e02bed26911403acd64511f314b7abd61161d
Slav666 <slawomirdyk@gmail.com>
2019-11-02 09:31:21 +0000

    TaskList function modified

--- src/components/App.js
+++ src/components/App.js
@@ -5,12 +5,33 @@ import TaskList from './TaskList';
 
 
 class App extends Component {
+
+  state = {
+    tasks: [
+      {
+      id: 0,
+      text: 'play in The Witcher 3 gamme',
+      date: '2019-11-01',
+      important: true,
+      active: true,
+      finishDate: null,
+    },
+    {
+      id: 0,
+      text: 'Learn React every day',
+      date: '2019-11-01',
+      important: true,
+      active: true,
+      finishDate: null,
+    },
+    ]
+  }
   render() {
     return (
       <div className="App">
       ToDo List
       <AddTask/>
-      <TaskList/>
+      <TaskList tasks={this.state.tasks}/>
       </div>
     );
   }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import Task from './Task'
 
-const TaskList = () => {
+const TaskList = (props) => {
+
+    const tasks = props.tasks.map(task => <Task key={task.id} task={task}/>)
     return ( 
-        <div>
-            <h2>Tasks List</h2>
-            <Task/>
-            <Task/>
-            <Task/>
-        </div>
+        <>
+            <div className="active">
+                <h1>Tasks to do</h1>
+                {tasks}
+            </div>
+            <hr/>
+            <div className="done">
+                <h2>Tasks finalised (0)</h2>
+            </div>
+        </>
         
     );
     
