35272d00f7f1a84d416ef3bbec7a78f19e0e3538
Slav666 <slawomirdyk@gmail.com>
2019-11-02 17:49:53 +0000

    If task imoprtant is true, this task is displayed in the browser in red colour

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,15 +1,27 @@
 import React from 'react';
 
 const Task = (props) => {
+
+    const style = {color: 'red'};
+
+    if (props.task.active) {
     return ( 
         <div>
             <p>
-            {props.task.text}<span>{props.task.date}</span><button onClick={() => props.changeStatus(props.task.id)}>I have done it</button><button onClick={() => props.delete(props.task.id)}>x</button>
+            <strong style={props.task.important ? style : null}>{props.task.text}</strong><span>{props.task.date}
+            </span><button onClick={() => props.changeStatus(props.task.id)}>I have done it</button>
+            <button onClick={() => props.delete(props.task.id)}>x</button>
             </p>
            
         </div>
            
-    );
+    );} else {
+        return(
+        <div>
+            Task done
+        </div>
+        )
+    }
     
 }
  
