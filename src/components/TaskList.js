5820ebcfb07e8a97c1c1f604adb6f1b138646953
Slav666 <slawomirdyk@gmail.com>
2019-11-02 16:48:23 +0000

    Code modified in TaskList so when a user click on I have done it button this task will be moved to the Tasks finalised section

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -3,16 +3,21 @@ import Task from './Task'
 
 const TaskList = (props) => {
 
-    const tasks = props.tasks.map(task => <Task key={task.id} task={task} delete={props.delete} changeStatus={props.changeStatus}/>)
+    const active = props.tasks.filter(task => task.active);
+    const done = props.tasks.filter(task => !task.active)
+
+    const activeTasks = active.map(task => <Task key={task.id} task={task} delete={props.delete} changeStatus={props.changeStatus}/>)
+    const doneTasks = done.map(task => <Task key={task.id} task={task} delete={props.delete} changeStatus={props.changeStatus}/>)
     return ( 
         <>
             <div className="active">
                 <h1>Tasks to do</h1>
-                {tasks}
+                {activeTasks}
             </div>
             <hr/>
             <div className="done">
                 <h2>Tasks finalised (0)</h2>
+                {doneTasks}
             </div>
         </>
         
