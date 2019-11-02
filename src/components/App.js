ba944db011799bcecf8a6a15208f2ce0c2ab81fa
Slav666 <slawomirdyk@gmail.com>
2019-11-02 08:21:09 +0000

    Add TaskList function

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import AddTask from './AddTask';
+import TaskList from './TaskList';
 
 
 class App extends Component {
@@ -9,6 +10,7 @@ class App extends Component {
       <div className="App">
       ToDo List
       <AddTask/>
+      <TaskList/>
       </div>
     );
   }
---
+++ src/components/TaskList.js
@@ -0,0 +1,15 @@
+
+import React, {Component} from 'react';
+
+class TaskList extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+               <div>This is TaskList</div> 
+            </div>
+         );
+    }
+}
+ 
+export default TaskList;
