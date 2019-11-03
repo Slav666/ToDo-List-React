e778d4cc5c7872460b983163f4d0e9eceadeee6b
Slav666 <slawomirdyk@gmail.com>
2019-11-03 08:52:03 +0000

    Create add task form with simple css

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,12 +1,22 @@
 
 import React, {Component} from 'react';
+import './AddTask.css'
 
 class  AddTask extends Component {
-    state = {  }
+    state = { 
+        text: '',
+        checked: false,
+     }
     render() { 
         return ( 
             <div className="form">
-                <div>Add task</div>
+                <input type="text" placeholder="Add Task" value={this.state.text} />
+                <input type="checkbox" checked={this.state.checked} id="important"/>
+                <label htmlFor="important">Priority</label>
+                <br/>
+                <label htmlFor="date">When should be finised:</label>
+                <input type="date" value="2019-11-03" min="2019-11-01" max="2020-12-01"/>
+                <button>ADD</button>
             </div>
          );
     }
