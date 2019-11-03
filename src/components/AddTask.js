2547103729a790faa925d2e55f389981d6716520
Slav666 <slawomirdyk@gmail.com>
2019-11-03 10:52:49 +0000

    minDate, maxDate and hadleDate method sorted

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -3,11 +3,22 @@ import React, {Component} from 'react';
 import './AddTask.css'
 
 class  AddTask extends Component {
+
+    minDate = new Date().toISOString().slice(0,10);
+
     state = { 
         text: '',
         checked: false,
+        date: this.minDate
+     }
+
+     handleDate = (e) => {
+        this.setState(
+            {date: e.target.value})
      }
     render() { 
+        let maxDate = this.minDate.slice(0,4) * 1 + 1;
+        maxDate = maxDate + "-12-31";
         return ( 
             <div className="form">
                 <input type="text" placeholder="Add Task" value={this.state.text} />
@@ -15,7 +26,7 @@ class  AddTask extends Component {
                 <label htmlFor="important">Priority</label>
                 <br/>
                 <label htmlFor="date">When should be finised:</label>
-                <input type="date" value="2019-11-03" min="2019-11-01" max="2020-12-01"/>
+                <input type="date" value={this.state.date} min={this.minDate} max={maxDate} onChange={this.handleDate}/>
                 <button>ADD</button>
             </div>
          );
