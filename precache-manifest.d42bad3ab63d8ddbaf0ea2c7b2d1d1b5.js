a02d1dc9f1277efc474025c923e071fd634a80eb
Slav666 <slawomirdyk@gmail.com>
2019-11-11 19:51:23 +0000

    Updates

---
+++ precache-manifest.d42bad3ab63d8ddbaf0ea2c7b2d1d1b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "456b66e7b7059e31914f",
+    "url": "/ToDo-List-React/static/js/runtime~main.bc285995.js"
+  },
+  {
+    "revision": "37faee4dcb0acee0036e",
+    "url": "/ToDo-List-React/static/js/main.738d6de7.chunk.js"
+  },
+  {
+    "revision": "3eb6f92a0c2c2fb8ce50",
+    "url": "/ToDo-List-React/static/js/2.240bb403.chunk.js"
+  },
+  {
+    "revision": "37faee4dcb0acee0036e",
+    "url": "/ToDo-List-React/static/css/main.01ce8ac7.chunk.css"
+  },
+  {
+    "revision": "333ae2bd356b9ad30c03353870e407e9",
+    "url": "/ToDo-List-React/index.html"
+  }
+];
